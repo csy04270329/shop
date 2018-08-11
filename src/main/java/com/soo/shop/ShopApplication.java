e5c1ef59bdab1198546b8a31eb6334ba6dbcf9c1
csy04270329 <tuchsoo3@gmail.com>
2018-08-12 01:27:23 +0900

    s

--- src/main/java/com/soo/shop/ShopApplication.java
+++ src/main/java/com/soo/shop/ShopApplication.java
@@ -3,10 +3,11 @@ package com.soo.shop;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ShopApplication {
+public class ShopApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
